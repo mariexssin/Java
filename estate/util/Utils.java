e7de7261399a2d258fc34f71f08a8fb57b1e7dae
mariexssin <strynadkomasha02.03@gmail.com>
2025-10-23 18:56:42 +0300

    Add utility methods for validation and formatting

---
+++ estate/util/Utils.java
@@ -0,0 +1,27 @@
+package ua.estate.util;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+public class Utils {
+    public static void validateString(String input, String fieldName) {
+        if (ValidationHelper.isStringInvalid(input)) {
+            throw new IllegalArgumentException(fieldName + " не може бути порожнім.");
+        }
+    }
+
+    public static void validatePositive(double value, String fieldName) {
+        if (ValidationHelper.isNegativeOrZero(value)) {
+            // ВАЛІДАЦІЯ
+            throw new IllegalArgumentException(fieldName + " має бути додатним числом.");
+        }
+    }
+    public static void validateEmail(String email) {
+        if (!ValidationHelper.isValidEmail(email)) {
+            throw new IllegalArgumentException("Некоректний формат email: " + email);
+        }
+    }
+
+    public static String formatCurrency(double price) {
+        return NumberFormat.getCurrencyInstance(Locale.US).format(price);
+    }
+}

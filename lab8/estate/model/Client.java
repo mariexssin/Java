b81320dcc9e6a81cfa51d9636876d94f227b60b7
mariexssin <strynadkomasha02.03@gmail.com>
2025-12-02 19:37:39 +0200

    Implement Client record with validation logic

---
+++ lab8/estate/model/Client.java
@@ -0,0 +1,20 @@
+package ua.estate.model;
+
+import ua.estate.util.EntityValidator;
+
+public record Client(String firstName, String lastName, String email) {
+
+    public Client {
+        EntityValidator validator = new EntityValidator("Client");
+
+        validator.checkString(firstName, "firstName");
+        validator.checkString(lastName, "lastName");
+        
+        validator.checkString(email, "email");
+        if (email != null && !email.contains("@")) {
+            validator.check(false, "email", "має некоректний формат");
+        }
+
+        validator.throwIfInvalid();
+    }
+}

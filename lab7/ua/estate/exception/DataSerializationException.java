99fb1e46bee25965678d3febd1cc95d481657504
mariexssin <strynadkomasha02.03@gmail.com>
2025-12-02 19:28:16 +0200

    Create DataSerializationException.java

---
+++ lab7/ua/estate/exception/DataSerializationException.java
@@ -0,0 +1,7 @@
+package ua.estate.exception;
+
+public class DataSerializationException extends RuntimeException {
+    public DataSerializationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}

178d014976b45de03d1349a5def6992e9d3a8106
mariexssin <strynadkomasha02.03@gmail.com>
2025-12-02 19:53:03 +0200

    Create Property.java

---
+++ lab9/estate/model/Property.java
@@ -0,0 +1,37 @@
+package ua.estate.model;
+
+import ua.estate.util.EntityValidator;
+import ua.estate.util.Utils;
+
+public record Property(String address, PropertyType type, double price, double area, PropertyStatus status) {
+
+    public Property {
+        EntityValidator validator = new EntityValidator("Property");
+        validator.checkString(address, "address");
+        validator.checkNotNull(type, "type");
+        validator.checkPositive(price, "price");
+        validator.checkPositive(area, "area");
+        validator.checkNotNull(status, "status");
+        validator.throwIfInvalid();
+    }
+
+    public double calculateCommission() {
+        double commissionRate = switch (type) {
+            case APARTMENT, HOUSE -> 0.05;
+            case OFFICE -> 0.08;
+            case LAND -> 0.03;
+        };
+        return price * commissionRate;
+    }
+
+    @Override
+    public String toString() {
+        return "Property{" +
+                "address='" + address + '\'' +
+                ", type=" + type +
+                ", price=" + Utils.formatCurrency(price) +
+                ", area=" + area +
+                ", status=" + status +
+                '}';
+    }
+}
